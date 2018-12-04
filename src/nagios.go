7f8d6871971600d228fa1912bb447cfcad22a93c
Camden Cheek <ccheek22@gmail.com>
2018-12-04 10:56:33 -0500

    Add labels

--- src/nagios.go
+++ src/nagios.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -24,22 +25,24 @@ const (
 	integrationVersion = "0.1.0"
 )
 
-var (
-	args ArgumentList
-)
-
-type ArgumentList struct {
+type argumentList struct {
 	sdkArgs.DefaultArgumentList
+	ServiceChecksConfig string
 }
 
-type ServiceCheckConfig struct {
-	ServiceChecks []struct {
-		Name    string   `yaml:"name"`
-		Command []string `yaml:"command"`
-	} `yaml:"service_checks"`
+type serviceCheckConfig struct {
+	ServiceChecks []serviceCheck `yaml:"service_checks"`
+}
+
+type serviceCheck struct {
+	Name    string            `yaml:"name"`
+	Command []string          `yaml:"command"`
+	Labels  map[string]string `yaml:"labels"`
 }
 
 func main() {
+	var args argumentList
+
 	// Create Integration
 	i, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
 	if err != nil {
@@ -48,62 +51,16 @@ func main() {
 
 	log.SetupLogging(args.Verbose)
 
-	yamlFile, err := ioutil.ReadFile("conf.yaml")
+	conf, err := parseConfigFile(args.ServiceChecksConfig)
 	if err != nil {
-		log.Error("Failed to read yaml file")
+		log.Error("Config parsing failed: %s", err.Error())
 		os.Exit(1)
 	}
 
-	var conf ServiceCheckConfig
-	err = yaml.Unmarshal(yamlFile, &conf)
-
-	for _, sc := range conf.ServiceChecks {
-		e, err := i.Entity(sc.Name, "serviceCheck")
-		if err != nil {
-			log.Error("Failed to get entity for service check %s: %s", sc.Name, err.Error())
+	if args.HasMetrics() {
+		for _, sc := range conf.ServiceChecks {
+			collectServiceCheck(sc, i)
 		}
-		out, exit, err := run_command(sc.Command)
-		if err != nil {
-			log.Error("Failed to run command %s: %s", sc.Name, err.Error())
-		}
-
-		ms := e.NewMetricSet("NagiosServiceCheckSample",
-			metric.Attribute{Key: "displayName", Value: sc.Name},
-			metric.Attribute{Key: "entityName", Value: "serviceCheck:" + sc.Name},
-		)
-
-		for _, metric := range []struct {
-			MetricName  string
-			MetricValue interface{}
-			MetricType  metric.SourceType
-		}{
-			{
-				"serviceCheck.name",
-				sc.Name,
-				metric.ATTRIBUTE,
-			},
-			{
-				"serviceCheck.status",
-				exit,
-				metric.GAUGE,
-			},
-			{
-				"serviceCheck.message",
-				out,
-				metric.ATTRIBUTE,
-			},
-			{
-				"serviceCheck.command",
-				strings.Join(sc.Command, " "),
-				metric.ATTRIBUTE,
-			},
-		} {
-			err := ms.SetMetric(metric.MetricName, metric.MetricValue, metric.MetricType)
-			if err != nil {
-				log.Error("Failed to set metric %s for %s: %s", metric.MetricName, sc.Name, err.Error())
-			}
-		}
-
 	}
 
 	// Create Entity
@@ -111,7 +68,81 @@ func main() {
 		log.Error("Failed to publish integration: %s", err.Error())
 		os.Exit(1)
 	}
+}
 
+func parseConfigFile(configFile string) (*serviceCheckConfig, error) {
+	yamlFile, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %s", err.Error())
+	}
+
+	var conf serviceCheckConfig
+	err = yaml.Unmarshal(yamlFile, &conf)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %s", err.Error())
+	}
+
+	return &conf, nil
+}
+
+func collectServiceCheck(sc serviceCheck, i *integration.Integration) {
+	e, err := i.Entity(sc.Name, "serviceCheck")
+	if err != nil {
+		log.Error("Failed to get entity for service check %s: %s", sc.Name, err.Error())
+	}
+	stdout, stderr, exit := runCommand(sc.Command[0], sc.Command[1:]...)
+	if err != nil {
+		log.Error("Failed to run command %s: %s", sc.Name, err.Error())
+	}
+
+	ms := e.NewMetricSet("NagiosServiceCheckSample",
+		metric.Attribute{Key: "displayName", Value: sc.Name},
+		metric.Attribute{Key: "entityName", Value: "serviceCheck:" + sc.Name},
+	)
+
+	for key, value := range sc.Labels {
+		err := ms.SetMetric(key, value, metric.ATTRIBUTE)
+		if err != nil {
+			log.Error("Failed to create label %s: %s", key, err.Error())
+		}
+	}
+
+	for _, metric := range []struct {
+		MetricName  string
+		MetricValue interface{}
+		MetricType  metric.SourceType
+	}{
+		{
+			"serviceCheck.name",
+			sc.Name,
+			metric.ATTRIBUTE,
+		},
+		{
+			"serviceCheck.status",
+			exit,
+			metric.GAUGE,
+		},
+		{
+			"serviceCheck.message",
+			stdout,
+			metric.ATTRIBUTE,
+		},
+		{
+			"serviceCheck.error",
+			stderr,
+			metric.ATTRIBUTE,
+		},
+		{
+			"serviceCheck.command",
+			strings.Join(sc.Command, " "),
+			metric.ATTRIBUTE,
+		},
+	} {
+		err := ms.SetMetric(metric.MetricName, metric.MetricValue, metric.MetricType)
+		if err != nil {
+			log.Error("Failed to set metric %s for %s: %s", metric.MetricName, sc.Name, err.Error())
+		}
+	}
 }
 
 func run_command(args []string) (string, int, error) {
@@ -140,4 +171,38 @@ func run_command(args []string) (string, int, error) {
 	}
 
 	return out.String(), exitCode, nil
+}
+
+func runCommand(name string, args ...string) (stdout string, stderr string, exitCode int) {
+	var outbuf, errbuf bytes.Buffer
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = &outbuf
+	cmd.Stderr = &errbuf
+
+	err := cmd.Run()
+	stdout = outbuf.String()
+	stderr = errbuf.String()
+
+	if err != nil {
+		// try to get the exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			ws := exitError.Sys().(syscall.WaitStatus)
+			exitCode = ws.ExitStatus()
+		} else {
+			// This will happen (in OSX) if `name` is not available in $PATH,
+			// in this situation, exit code could not be gotten, and stderr will likely
+			// be an empty string, so we use the default fail code, and format err
+			// to string and set to stderr
+			exitCode = -1
+			if stderr == "" {
+				stderr = err.Error()
+			}
+		}
+	} else {
+		// success, exitCode should be 0 if go is ok
+		ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
+		exitCode = ws.ExitStatus()
+	}
+
+	return
 }
